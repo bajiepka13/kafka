833a568cc5db2fdc48ee6322a29a5ac0f015274f
Валерий Чаленко <vchalenko@dz.ru>
2019-06-24 17:28:24 +0300

    initial commit

---
+++ src/main/java/org/bajiepka/kafka/kafkaproducer/controller/UserController.java
@@ -0,0 +1,44 @@
+package org.bajiepka.kafka.kafkaproducer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    private final String KAFKA_TOPIC = "common";
+    @Autowired
+    KafkaTemplate<String, String> kafkaTemplate;
+
+    @GetMapping("/publish/{message}")
+    public String post(@PathVariable("message") final String message) {
+
+        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(KAFKA_TOPIC, message);
+
+        future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                System.out.println("Sent message=[" + message +
+                        "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                System.out.println("Unable to send message=["
+                        + message + "] due to : " + ex.getMessage());
+            }
+        });
+
+        return "Published succesfully!";
+    }
+
+}
